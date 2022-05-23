b0475f7d2fc1039c1ae5fd0ab3e7a6cecc60c830
phani051 <phani051@gmail.com>
2022-05-23 19:52:27 +0530

    Working

---
+++ game.js
@@ -0,0 +1,105 @@
+// **********  Variables  ********
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+// ******* Start Game *******
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+// ******* User input and answer check *******
+
+
+$(".btn").click(function () {
+  userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+// ********** Function for checking Answer *********
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length) {
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      console.log("fail");
+      var audio = new Audio("./sounds/wrong.mp3");
+  audio.play();
+      $("body").addClass("game-over");
+      setTimeout(function() {
+        $("body").removeClass("game-over");
+      },200);
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+  }
+  
+
+
+// ********** Function for Next Sequence *********
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  console.log(gamePattern);
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+  
+}
+
+
+// ******* Function to Play Sound ********
+
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+// ******* Button Click Animation ********
+
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+// ******* Function for start over ********
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+
+}
+
